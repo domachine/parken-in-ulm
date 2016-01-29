6f9d854188ad7760b4165aff25f575ecd6ed2a44
Dominik Burgdörfer <db@domachine.de>
2016-01-29 15:12:44 +0100

    Add frontend mock

--- server.js
+++ server.js
@@ -3,10 +3,13 @@
 const util = require('util');
 const http = require('http');
 const express = require('express');
+const rewrite = require('connect-modrewrite');
 
 const app = express();
 
 app.use('/api', require('./api'));
+app.use(rewrite(['^[^.]*$ /index.html']));
+app.use(express.static(`${__dirname}/static`));
 
 http.createServer(app).listen(process.env.PORT || 3000, () => {
   util.log('Listening ...');
