b3df9bbc401798778d5abc7b25ec112b74a71993
Dominik Burgdörfer <db@domachine.de>
2016-01-30 10:41:24 +0100

    Factor overview into own container

--- containers/app/index.jsx
+++ containers/app/index.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import { Appbar, Container, Panel, TextInput } from 'muicss/react';
+import { Appbar, Container, Panel } from 'muicss/react';
 import request from 'superagent';
-import { createSelector } from 'reselect';
+
+import Overview from 'containers/overview';
 
 export default class App extends Component {
   constructor() {
     super();
 
-    this.onChangeSearchString = this.onChangeSearchString.bind(this);
-
-    this.visibleParkinglots = createSelector(
-      state => state.parkinglots,
-      state => state.searchString,
-      (parkinglots, searchString) =>
-        parkinglots.filter(lot =>
-          lot.name.toLowerCase().indexOf(searchString) !== -1
-        )
-    );
     this.state = {
       isFetching: true,
       parkinglots: [],
@@ -26,10 +17,6 @@ export default class App extends Component {
 
   componentDidMount() {
     this.fetchParkinglots();
-  }
-
-  onChangeSearchString(e) {
-    this.setState({ searchString: e.target.value });
   }
 
   fetchParkinglots() {
@@ -55,39 +42,7 @@ export default class App extends Component {
                 <div className="mui--text-center mui--text-button">Lade ...</div>
               </Panel>
             ) : null}
-          <Panel>
-            <TextInput
-              hint="Suchen ..."
-              onChange={this.onChangeSearchString}
-              value={this.state.searchString}
-            />
-          </Panel>
-          {this.visibleParkinglots(this.state).map(parkinglot =>
-            <Panel>
-              <div className="mui--text-title">
-                {parkinglot.name}
-              </div>
-              <br />
-              <div className="mui--text-caption">
-                geöffnet
-              </div>
-              <div className="mui--text-body1">
-                <span className="mui--text-body2">
-                  {parkinglot.open}
-                </span>
-              </div>
-              <br />
-              <div className="mui--text-caption">
-                freie Plätze
-              </div>
-              <div>
-                <span className="mui--text-body2">
-                  {parkinglot.available || 0}
-                </span>
-                <span className="mui--text-caption mui--text-dark-hint"> / {parkinglot.total}</span>
-              </div>
-            </Panel>
-          )}
+          <Overview parkinglots={this.state.parkinglots} />
         </Container>
       </div>
     );
---
+++ containers/overview/index.jsx
@@ -0,0 +1,73 @@
+import React, { Component, PropTypes } from 'react';
+import { Panel, TextInput } from 'muicss/react';
+import { createSelector } from 'reselect';
+
+export default class Overview extends Component {
+  constructor() {
+    super();
+
+    this.onChangeSearchString = this.onChangeSearchString.bind(this);
+
+    // Selector to filter parkinglots using search-string
+    this.visibleParkinglots = createSelector(
+      (state, props) => props.parkinglots,
+      state => state.searchString,
+      (parkinglots, searchString) =>
+        parkinglots.filter(lot =>
+          lot.name.toLowerCase().indexOf(searchString) !== -1
+        )
+    );
+
+    this.state = {
+      searchString: '',
+    };
+  }
+
+  onChangeSearchString(e) {
+    this.setState({ searchString: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Panel>
+          <TextInput
+            hint="Suchen ..."
+            onChange={this.onChangeSearchString}
+            value={this.state.searchString}
+          />
+        </Panel>
+        {this.visibleParkinglots(this.state, this.props).map(parkinglot =>
+          <Panel key={parkinglot.id}>
+            <div className="mui--text-title">
+              {parkinglot.name}
+            </div>
+            <br />
+            <div className="mui--text-caption">
+              geöffnet
+            </div>
+            <div className="mui--text-body1">
+              <span className="mui--text-body2">
+                {parkinglot.open}
+              </span>
+            </div>
+            <br />
+            <div className="mui--text-caption">
+              freie Plätze
+            </div>
+            <div>
+              <span className="mui--text-body2">
+                {parkinglot.available || 0}
+              </span>
+              <span className="mui--text-caption mui--text-dark-hint"> / {parkinglot.total}</span>
+            </div>
+          </Panel>
+        )}
+      </div>
+    );
+  }
+}
+
+Overview.propTypes = {
+  parkinglots: PropTypes.array.isRequired,
+};
